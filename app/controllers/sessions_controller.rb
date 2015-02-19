0d5c1f3b969bae305735c29ecceac99867eda355
tylore <hymanson@gmail.com>
2015-02-18 17:10:54 -0800

    Functional Log Out Added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     e = Explorer.where(email: params[:explorer][:email]).first
-    if e != nil && u.authenticate(params[:explorer][:password])
+    if e != nil && e.authenticate(params[:explorer][:password])
       session["explorer_id"] = e.id.to_s
-      redirect_to adventures_path(explorer_id: u.id)
+      redirect_to adventures_path(explorer_id: e.id)
     else
       redirect_to new_sessions_path
     end
